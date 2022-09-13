076f8da6a1781d574ac5b69f1bf100eb8a2387a9
gocool94 <gkalivar@cisco.com>
2022-09-13 09:25:19 +0530

    day started

--- FullStackWebDeveloper/Python Refresher/typehinting.py
+++
@@ -1,4 +0,0 @@
-number = 7
-
-#Guess number 
-user_input = input("Enter Y if you want to play")
---
+++ New folder/variables.py
@@ -0,0 +1,9 @@
+amount = 100
+
+value = 50
+
+gst = 5
+
+total = amount + value + gst
+
+print(total)
